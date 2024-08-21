9262c3edc425224a1607c24243e017b80de9add9
schinhsu <s710409@hotmail.com.tw>
2024-08-21 11:35:58 +0800

    Modify the condition while tracing

--- function/trace_tx2.py
+++ function/trace_tx2.py
@@ -15,7 +15,9 @@ def get_target_txs(txinfo,traceType,toTrace,traceTolerance=10,ignoreAmount=1):
             start = True
             continue
         if start:
-            if row[traceType] != txinfo[traceType] and row['Contract'] == txinfo['Contract'] and row['Token'] == txinfo['Token']:
+            ### eth的Contract欄位會是空值
+            if row[traceType] != txinfo[traceType] and (len(row['Contract']) == 0 or row['Contract'] == txinfo['Contract']) and row['Token'] == txinfo['Token']:
+            #if row[traceType] != txinfo[traceType] and row['Contract'] == txinfo['Contract'] and row['Token'] == txinfo['Token']:
                 amount += row['Value']
                 count += 1
                 result = pandas.concat([result,pandas.DataFrame([row.values],columns=row.index)])
